0440f85ff492da0fe0014f4fd2014f2831d09c62
¨ansh¨ <anshballu7@gmail.com>
2025-04-12 23:21:44 +0100

    added edlint to not treat warnings as errors in deployment

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
+    plugins: [
+      'jsx-a11y'
+    ],
     rules: {
-      'jsx-a11y/anchor-is-valid': 'warn',  // Downgrade from error to warning
+      'jsx-a11y/anchor-is-valid': 'warn', // Downgrade from error to warning
       'jsx-a11y/anchor-has-content': 'warn'
     }
   };
